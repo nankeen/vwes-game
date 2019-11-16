c50263a2ed43f3b04365390b79883889ba9e1e78
04khey <PenguinProductions00@gmail.com>
2019-11-16 14:23:42 +0000

    Added simple seek script

---
+++ Assets/Scripts/RunToBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunToBall : MonoBehaviour
+{
+    public GameObject Ball;
+    public int movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Ball.transform);
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+
+    }
+}
