9554df1cc781b11cd10eba006d7fc5ed5ea633e6
04khey <PenguinProductions00@gmail.com>
2019-11-16 14:58:42 +0000

    Fixed movement along plane, sprite issues

--- Assets/Scripts/RunToBall.cs
+++ Assets/Scripts/RunToBall.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class RunToBall : MonoBehaviour
 {
     public GameObject Ball;
-    public int movementSpeed;
+    public float movementSpeed;
+    public Vector3 toBall;
+    public Camera cam;
+    private Vector3 courtPlaneNormal = Vector3.up;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +18,9 @@ public class RunToBall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Ball.transform);
-        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        transform.LookAt(cam.transform);
+        toBall = Ball.transform.position - transform.position;
+        transform.position += Vector3.Normalize(Vector3.ProjectOnPlane(toBall, courtPlaneNormal)) * movementSpeed * Time.deltaTime;
 
     }
 }
