27ac5a64a7142ebf17299bd4960aa24724dc8ee1
04khey <PenguinProductions00@gmail.com>
2019-11-17 00:28:17 +0000

    adding scoring

---
+++ Assets/Scripts/CourtCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CourtCollision : MonoBehaviour
+{
+    public GameObject CourtSideDetector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Ball") //if ball hit the court
+        {
+            CourtSideDetector.GetComponent<ScoringLogic>().ballHasCollided = true;
+        }
+    }
+}
